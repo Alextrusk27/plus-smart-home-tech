4682225a78c7b8c72e572b5939155fb8518d8543
Nikolay Aleksandrov <nkl.aleks@yandex.ru>
2026-04-19 18:31:50 +0300

    refactor: remove unnecessary NOT NULL restrictions in order schema.sql

--- commerce/order/src/main/resources/schema.sql
+++ commerce/order/src/main/resources/schema.sql
@@ -1,16 +1,16 @@
 CREATE TABLE IF NOT EXISTS orders (
-    order_id         UUID PRIMARY KEY,
+    order_id         UUID         PRIMARY KEY,
     username         VARCHAR(255) NOT NULL,
-    shopping_cart_id UUID NOT NULL UNIQUE,
-    payment_id       UUID,
+    shopping_cart_id UUID         NOT NULL UNIQUE,
+    payment_id       UUID         NOT NULL UNIQUE,
     delivery_id      UUID,
-    state            VARCHAR(20) NOT NULL,
+    state            VARCHAR(20)  NOT NULL,
     delivery_weight  DECIMAL(6,2) NOT NULL,
     delivery_volume  DECIMAL(6,2) NOT NULL,
-    delivery_price   DECIMAL(6,2) NOT NULL,
-    fragile          BOOL NOT NULL,
-    product_price    DECIMAL(6,2) NOT NULL,
-    total_price      DECIMAL(6,2) NOT NULL,
+    delivery_price   DECIMAL(6,2),
+    fragile          BOOL         NOT NULL,
+    product_price    DECIMAL(6,2),
+    total_price      DECIMAL(6,2),
 
     CONSTRAINT chk_state CHECK (state IN ('NEW', 'ON_PAYMENT', 'ON_DELIVERY', 'ON_ASSEMBLY', 'DELIVERED', 'ASSEMBLED',
         'PAID', 'COMPLETED', 'DELIVERY_FAILED', 'ASSEMBLY_FAILED', 'PAYMENT_FAILED', 'PRODUCT_RETURNED', 'CANCELED')),
@@ -30,9 +30,6 @@ CREATE TABLE IF NOT EXISTS order_products (
    PRIMARY KEY (order_id, product_id),
    CONSTRAINT chk_quantity_positive CHECK (quantity > 0)
 );
-
-CREATE UNIQUE INDEX idx_unique_payment_id ON orders(payment_id)
-WHERE payment_id IS NOT NULL;
 
 CREATE UNIQUE INDEX idx_unique_delivery_id ON orders(delivery_id)
 WHERE delivery_id IS NOT NULL;
